457c51020cde3de464336095fc7d86b5eefeba5f
Tu Ngo <tungo8810@gmail.com>
2017-04-12 11:05:40 -0700

    Add associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,21 @@
 class Course < ActiveRecord::Base
+
+  has_many :enrollments,
+            primary_key: :id,
+            foreign_key: :course_id,
+            class_name: 'Enrollment'
+
+  has_many :enrolled_students,
+            through: :enrollments,
+            source: :students
+
+  belongs_to :prerequisite,
+           primary_key: :id,
+           foreign_key: :prereq_id,
+           class_name: 'Course'
+
+  has_one :instructor,
+          primary_key: :instructor_id,
+          foreign_key: :id,
+          class_name: "User"
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,11 @@
 class Enrollment < ActiveRecord::Base
+  belongs_to :students,
+             primary_key: :id,
+             foreign_key: :student_id,
+             class_name: "User"
+
+  belongs_to :course,
+             primary_key: :id,
+             foreign_key: :course_id,
+             class_name: "Course"
 end
